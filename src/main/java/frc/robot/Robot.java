9808886530b41a6f07efedb4bd24229c65a98372
Henry Scheible <henry.scheible@gmail.com>
2020-04-13 12:53:07 -0500

    Add a new feature

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -31,6 +31,7 @@ public class Robot extends TimedRobot
     @Override
     public void robotInit()
     {
+        //random feature
         // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
         // autonomous chooser on the dashboard.
         robotContainer = new RobotContainer();
