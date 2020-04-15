959e85627a32dfa139c980c19891240ead4f6da5
missannikab <63022891+missannikab@users.noreply.github.com>
2020-04-15 18:05:51 -0500

    Update MoveClimbCommand.java

--- src/main/java/frc/robot/commands/MoveClimbCommand.java
+++ src/main/java/frc/robot/commands/MoveClimbCommand.java
@@ -8,6 +8,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Climb;
 
 public class MoveClimbCommand extends CommandBase {
   private Climb subsystem;
