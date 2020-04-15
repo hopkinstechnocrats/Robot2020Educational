16a02f00ab0ad5e36024f80cae27b7d98e0db46a
missannikab <63022891+missannikab@users.noreply.github.com>
2020-04-15 18:01:23 -0500

    Update MoveClimbCommand.java

--- src/main/java/frc/robot/commands/MoveClimbCommand.java
+++ src/main/java/frc/robot/commands/MoveClimbCommand.java
@@ -30,7 +30,7 @@ public class MoveClimbCommand extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    subsystem.moveClimb(double speed);
+    subsystem.moveClimb(speed);
   }
 
   // Called once the command ends or is interrupted.
