dbc437b97ba84af78d4b813604f8b81c1bfe1cc3
HenryWeinman <63022890+HenryWeinman@users.noreply.github.com>
2020-04-15 21:07:37 -0500

    IntakeBall/JoystickDriveCommand
    I hope this works

---
+++ src/main/java/frc/robot/commands/JoystickDriveCommand.java
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
+/* Open Source Software - may be modified and shared by FRC teams. The code   */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project.                                                               */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.Drivetrain;
+
+
+public class JoystickDriveCommand extends CommandBase {
+  private Drivetrain subsystem;
+  private XboxController controller;
+
+  public JoystickDriveCommand(Drivetrain subsystem, XboxController controller) {
+    this.controller = controller;
+    this.subsystem = subsystem;
+    addRequirements(subsystem);
+  }
+
+  @Override
+  public void execute() {
+    subsystem.sendDriveSignal(controller.getRawAxis(0), controller.getRawAxis(1));
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
